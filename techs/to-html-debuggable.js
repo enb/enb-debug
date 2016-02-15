4f113506587335898954a60494028de90699a265
Alexey Yaroshevich <zxqfox@gmail.com>
2016-02-16 00:06:23 +0300

    fix(to-html-debuggable): async enb logs capturing

--- techs/to-html-debuggable.js
+++ techs/to-html-debuggable.js
@@ -3,18 +3,26 @@ var concap = require('concap');
 module.exports.using = function(baseTech) {
     return baseTech.buildFlow()
         .name('to-html-debuggable')
+        .methods({
+            render: function () {
+                var render = this.__base.apply.bind(this, arguments);
+                var res = concap.capture(render);
+                this._concapped = concap.getClean();
+                return res;
+            }
+        })
         .builder(function(files) {
-            concap.hijack();
+            var _this = this;
             return this.__base.apply(this, arguments)
                 .catch(function(err) {
                     return "<pre>" + err.stack + "</pre>";
                 })
                 .then(function(res) {
-                    concap.restore();
-                    return res +
+                    var log = _this._concapped ?
                         '<script type="text/javascript">\n;' +
-                            concap.render(concap.getClean()) +
-                        '\n<\/script>\n';
+                            concap.render(_this._concapped) +
+                        '\n<\/script>\n' : '';
+                    return res + log;
                 });
         })
         .createTech();
